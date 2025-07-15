697cf636aa67234e12c986d8365533b7488678df
Narutino10 <ibrahim.ouahabi@outlook.fr>
2025-07-15 12:21:02 +0200

    correction Register, et recipe

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,5 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsString,
+  MinLength,
+  Matches,
+  IsArray,
+  ArrayNotEmpty,
+  IsIn,
+} from 'class-validator';
 
 export class RegisterDto {
   @ApiProperty()
@@ -8,6 +16,25 @@ export class RegisterDto {
 
   @ApiProperty()
   @IsString()
-  @MinLength(6)
+  @MinLength(8)
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).+$/,
+    {
+      message:
+        'Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial.',
+    },
+  )
   password: string;
+
+  @ApiProperty({
+    isArray: true,
+    example: ['user'],
+    enum: ['user', 'admin'],
+    description: 'Rôles à attribuer (user ou admin)',
+    default: ['user'],
+  })
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsIn(['user', 'admin'], { each: true })
+  roles: string[];
 }
